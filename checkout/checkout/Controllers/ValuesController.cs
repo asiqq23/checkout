cbd82273dc64f34115d30b8407469c365090c546
Adrian Zawadzki <adrian.zawadzki@fabres.pl>
2017-08-04 12:54:40 +0200

    init

---
+++ checkout/checkout/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace checkout.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"Checkout on {Environment.MachineName} at {DateTime.Now}";
+        }
+    }
+}
